f63fd4833ba861b943be53276cd183e8d1d12ea9
Ly Hoang Thuan <hoangthuann070@gmail.com>
2022-08-27 23:41:58 +0700

    Fix Routing bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'pagenotfound', component: PageNotFoundComponent }
+    {path: 'about', component: AboutComponent}
 ];
 
 
@@ -17,4 +15,4 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 
-export class AppRountingModule { }
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -19,7 +19,7 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule, 
     FormsModule,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
